6e1ab8f3e92450a2db14f4530e70772b2c106d5b
Rafael Philippou <rafaelphilippou@gmail.com>
2026-03-01 17:11:45 +0200

    Added game ending and color adjustment
    Game now ends when a player reaches 7 score. Players also have the ability to restart the game.
    
    Adjusted some player and map colors.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    [SerializeField] int scoreToEnd = 7; //Score needed for the game to end
+    [Space]
     [SerializeField] int player1Score;
     [SerializeField] int player2Score;
 
@@ -18,13 +21,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] Transform player2Spawn;
     [Space]
     [SerializeField] float resetYPosition = -10;
+    [Space]
+    [SerializeField] GameObject endCanvas;
+    [SerializeField] GameObject player1WinsText;
+    [SerializeField] GameObject player2WinsText;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
@@ -41,6 +46,28 @@ public class GameManager : MonoBehaviour
             OnPlayerScored?.Invoke(player1Score, player2Score);
             SpawnPlayers();
         }
+
+        if (player1Score == scoreToEnd || player2Score == scoreToEnd)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        player1.GetComponent<Rigidbody>().isKinematic = true;
+        player2.GetComponent<Rigidbody>().isKinematic = true;
+
+        endCanvas.SetActive(true);
+
+        if (player1Score > player2Score)
+        {
+            player1WinsText.SetActive(true);
+        }
+        else
+        {
+            player2WinsText.SetActive(true);
+        }
     }
 
     void SpawnPlayers()
@@ -51,5 +78,10 @@ public class GameManager : MonoBehaviour
         player2.position = player2Spawn.position;
         player2.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         player2.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
